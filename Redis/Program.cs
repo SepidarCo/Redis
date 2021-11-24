e0a6673d95c722dbe02486d259f2012ee52669c5
ali fallah <ali_fhl@yahoo.com>
2021-11-24 16:19:38 +0330

    fixing bug

--- Redis/Program.cs
+++ Redis/Program.cs
@@ -14,24 +14,17 @@ namespace Redis
             people.Add(new Person(3, "nazanin"));
             people.Add(new Person(4, "shahrooz"));
 
-<<<<<<< HEAD
+
             RedisProvider.Instance.SetCacheData(people, "People");
-=======
-             RedisProvider.Instance.SetCacheData(people, "People");
 
-
->>>>>>> 767e880924f652ead0cd18afd55b91a8c2ee3199
+            //RedisProvider.Instance.SetCacheData(people, "People");
 
             //RedisProvider.Instance.RemoveAllData<Person>("People");
 
-            //var model = RedisProvider.Instance.GetAllData<List<Person>>("People");
+            var model = RedisProvider.Instance.GetAllData<List<Person>>("People");
 
-<<<<<<< HEAD
             Console.WriteLine(model);
 
-=======
-            //Console.WriteLine(model);
->>>>>>> 767e880924f652ead0cd18afd55b91a8c2ee3199
             Console.ReadKey();
 
         }
